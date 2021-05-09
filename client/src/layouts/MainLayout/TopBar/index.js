6f9d59b1cffc1a4302b718a49308f9aef2f2f8d5
stinso <marc.hilzendecker@gmail.com>
2021-05-10 00:01:11 +0200

    responsive appbar

--- client/src/layouts/MainLayout/TopBar/index.js
+++ client/src/layouts/MainLayout/TopBar/index.js
@@ -28,35 +28,6 @@ import SearchIcon from '@material-ui/icons/Search';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 
-const selectOptions = [
-  {
-    label: 'Type',
-    options: [
-      'Freelance',
-      'Full Time',
-      'Part Time',
-      'Internship']
-  },
-  {
-    label: 'Level',
-    options: ['Novice', 'Expert']
-  },
-  {
-    label: 'Location',
-    options: [
-      'Africa',
-      'Asia',
-      'Australia',
-      'Europe',
-      'North America',
-      'South America'
-    ]
-  },
-  {
-    label: 'Roles',
-    options: ['Android', 'Web Developer', 'iOS']
-  }
-];
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -127,129 +98,177 @@ const TopBar = ({ className, onNavOpen, ...rest }) => {
       {...rest}
     >
       <Toolbar className={classes.toolbar}>
-        <Grid container className={classes.root}>
-          <Grid item xs={4}>
-            <Grid container justify="flex-start">
-              <Box display="flex">
-                <Box mr={2}>
-                  <Button
-                    color="secondary"
-                    href="https://app.uniswap.org/#/swap?outputCurrency=0xC4C2614E694cF534D407Ee49F8E44D125E4681c4"
-                    variant="outlined"
-                  >
-                    Buy Tokens
-                  </Button>
-                </Box>
-                <Button
-                  aria-controls="menu-shooter"
-                  aria-haspopup="true"
-                  onClick={handleMenuShooter}
+          <Hidden lgUp>
+            <IconButton
+              id="logo"
+              //onClick={onMobileNavOpen}
+            >
+              <SvgIcon
+                fontSize="small"
+              >
+                <MenuIcon />
+              </SvgIcon>
+            </IconButton>
+            <Box
+              ml={2}
+              flexGrow={1}
+            />
+            <Box display="flex">
+              <IconButton aria-label="search" color="inherit">
+                <SearchIcon />
+              </IconButton>
+              <IconButton aria-label="show 17 new notifications" color="inherit">
+                <Badge badgeContent={17} color="secondary">
+                  <NotificationsIcon />
+                </Badge>
+              </IconButton>
+              <Box ml={2} display="flex">
+                <Button 
+                  className={classes.root}
+                  onClick={onNavOpen}
                 >
-                  Shooter
-                  <ArrowDropDownIcon />
-                </Button>
-                <Menu
-                  id="menu-shooter"
-                  anchorEl={anchorElShooter}
-                  getContentAnchorEl={null}
-                  anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
-                  transformOrigin={{ vertical: "top", horizontal: "center" }}
-                  keepMounted
-                  open={openShooter}
-                  onClose={handleCloseShooter}
-                >
-                  <MenuItem onClick={handleCloseShooter} color="#fff" component={RouterLink} to="/actionGamePage/cod">Call of Duty</MenuItem>
-                </Menu>
-                
-                <Button
-                  aria-controls="menu-sports"
-                  aria-haspopup="true"
-                  onClick={handleMenuSports}
-                >
-                  Sports
-                  <ArrowDropDownIcon />
-                </Button>
-                <Menu
-                  id="menu-sports"
-                  anchorEl={anchorElSports}
-                  getContentAnchorEl={null}
-                  anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
-                  transformOrigin={{ vertical: "top", horizontal: "center" }}
-                  keepMounted
-                  open={openSports}
-                  onClose={handleCloseSports}
-                >
-                  <MenuItem onClick={handleCloseSports} color="#fff" component={RouterLink} to="/actionGamePage/fifa">Fifa</MenuItem>
-                  <MenuItem onClick={handleCloseSports} color="#fff" component={RouterLink} to="/actionGamePage/madden">Madden</MenuItem>
-                </Menu>
-                <Button
-                  aria-controls="menu-casual"
-                  aria-haspopup="true"
-                  onClick={handleMenuCasual}
-                >
-                  Casual
-                  <ArrowDropDownIcon />
-                </Button>
-                <Menu
-                  id="menu-casual"
-                  anchorEl={anchorElCasual}
-                  getContentAnchorEl={null}
-                  anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
-                  transformOrigin={{ vertical: "top", horizontal: "center" }}
-                  keepMounted
-                  open={openCasual}
-                  onClose={handleCloseCasual}
-                >
-                  <MenuItem onClick={handleCloseCasual} color="#fff" component="a" href="https://chaino.chaingames.io/">Chaino</MenuItem>
-                  <MenuItem onClick={handleCloseCasual} color="#fff" component="a" href="https://chaingames.io/#sck">Super Crypto Kart</MenuItem>
-                </Menu>
-              </Box>
-            </Grid>
-          </Grid>
-          <Grid item xs={4}>
-            <Grid container justify="center">
-              <RouterLink to="/">
-                <Logo />
-              </RouterLink>
-            </Grid>
-          </Grid>
-          <Grid item xs={4}>
-            <Grid container justify="flex-end" spacing={2}>
-              <Box display="flex">
-                <IconButton aria-label="search" color="inherit">
-                  <SearchIcon />
-                </IconButton>
-                <IconButton aria-label="show 17 new notifications" color="inherit">
-                  <Badge badgeContent={17} color="secondary">
-                    <NotificationsIcon />
-                  </Badge>
-                </IconButton>
-                <Box ml={2} display="flex">
-                  <Button 
-                    className={classes.root}
-                    onClick={onNavOpen}
-                  >
-                    <Box mr={1}>
-                      <Typography
-                        variant="body1"
-                        color="textPrimary"
-                      >
-                        mukki
-                      </Typography>
-                    </Box>
-                    <SvgIcon
-                      fontSize="small"
+                  <Box mr={1}>
+                    <Typography
+                      variant="body1"
                       color="textPrimary"
                     >
-                      <MenuIcon />
-                    </SvgIcon>
-                  </Button>
-                </Box>
+                      mukki
+                    </Typography>
+                  </Box>
+                  <SvgIcon
+                    fontSize="small"
+                    color="textPrimary"
+                  >
+                    <MenuIcon />
+                  </SvgIcon>
+                </Button>
               </Box>
+            </Box>
+          </Hidden>
+          <Hidden mdDown>
+            <Grid container className={classes.root}>
+              <Grid item xs={4}>
+                <Grid container justify="flex-start">
+                  <Box display="flex">
+                    <Box mr={2}>
+                      <Button
+                        color="secondary"
+                        href="https://app.uniswap.org/#/swap?outputCurrency=0xC4C2614E694cF534D407Ee49F8E44D125E4681c4"
+                        variant="outlined"
+                      >
+                        Buy Tokens
+                      </Button>
+                    </Box>
+                    <Button
+                      aria-controls="menu-shooter"
+                      aria-haspopup="true"
+                      onClick={handleMenuShooter}
+                    >
+                      Shooter
+                      <ArrowDropDownIcon />
+                    </Button>
+                    <Menu
+                      id="menu-shooter"
+                      anchorEl={anchorElShooter}
+                      getContentAnchorEl={null}
+                      anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+                      transformOrigin={{ vertical: "top", horizontal: "center" }}
+                      keepMounted
+                      open={openShooter}
+                      onClose={handleCloseShooter}
+                    >
+                      <MenuItem onClick={handleCloseShooter} color="#fff" component={RouterLink} to="/actionGamePage/cod">Call of Duty</MenuItem>
+                    </Menu>
+                    
+                    <Button
+                      aria-controls="menu-sports"
+                      aria-haspopup="true"
+                      onClick={handleMenuSports}
+                    >
+                      Sports
+                      <ArrowDropDownIcon />
+                    </Button>
+                    <Menu
+                      id="menu-sports"
+                      anchorEl={anchorElSports}
+                      getContentAnchorEl={null}
+                      anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+                      transformOrigin={{ vertical: "top", horizontal: "center" }}
+                      keepMounted
+                      open={openSports}
+                      onClose={handleCloseSports}
+                    >
+                      <MenuItem onClick={handleCloseSports} color="#fff" component={RouterLink} to="/actionGamePage/fifa">Fifa</MenuItem>
+                      <MenuItem onClick={handleCloseSports} color="#fff" component={RouterLink} to="/actionGamePage/madden">Madden</MenuItem>
+                    </Menu>
+                    <Button
+                      aria-controls="menu-casual"
+                      aria-haspopup="true"
+                      onClick={handleMenuCasual}
+                    >
+                      Casual
+                      <ArrowDropDownIcon />
+                    </Button>
+                    <Menu
+                      id="menu-casual"
+                      anchorEl={anchorElCasual}
+                      getContentAnchorEl={null}
+                      anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+                      transformOrigin={{ vertical: "top", horizontal: "center" }}
+                      keepMounted
+                      open={openCasual}
+                      onClose={handleCloseCasual}
+                    >
+                      <MenuItem onClick={handleCloseCasual} color="#fff" component="a" href="https://chaino.chaingames.io/">Chaino</MenuItem>
+                      <MenuItem onClick={handleCloseCasual} color="#fff" component="a" href="https://chaingames.io/#sck">Super Crypto Kart</MenuItem>
+                    </Menu>
+                  </Box>
+                </Grid>
+              </Grid>
+              <Grid item xs={4}>
+                <Grid container justify="center">
+                  <RouterLink to="/dashboard">
+                    <Logo />
+                  </RouterLink>
+                </Grid>
+              </Grid>
+              <Grid item xs={4}>
+                <Grid container justify="flex-end" spacing={2}>
+                  <Box display="flex">
+                    <IconButton aria-label="search" color="inherit">
+                      <SearchIcon />
+                    </IconButton>
+                    <IconButton aria-label="show 17 new notifications" color="inherit">
+                      <Badge badgeContent={17} color="secondary">
+                        <NotificationsIcon />
+                      </Badge>
+                    </IconButton>
+                    <Box ml={2} display="flex">
+                      <Button 
+                        className={classes.root}
+                        onClick={onNavOpen}
+                      >
+                        <Box mr={1}>
+                          <Typography
+                            variant="body1"
+                            color="textPrimary"
+                          >
+                            mukki
+                          </Typography>
+                        </Box>
+                        <SvgIcon
+                          fontSize="small"
+                          color="textPrimary"
+                        >
+                          <MenuIcon />
+                        </SvgIcon>
+                      </Button>
+                    </Box>
+                  </Box>
+                </Grid>
+              </Grid>
             </Grid>
-          </Grid>
-        </Grid>
-
+          </Hidden>
       </Toolbar>
     </AppBar>
   );
