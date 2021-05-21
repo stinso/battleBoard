61aa53924a172ff18ce4448c2f578b03f52d4bae
stinso <marc.hilzendecker@gmail.com>
2021-05-21 21:27:52 +0200

    cleanup

--- src/hooks/useConnectedWallet.js
+++
@@ -1,6 +0,0 @@
-import useLocalStorage from './useLocalStorage';
-
-export default function useConnectedWallet() {
-  const [ connectedWallet, setConnectedWallet] = useLocalStorage('connected_wallet', '')
-  return [connectedWallet, setConnectedWallet]
-}
